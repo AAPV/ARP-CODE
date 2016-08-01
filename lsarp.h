6e43c03d909453c1db7a00c8eeb8382b5a2e2fc9
Peter Doherty <peter_doherty@Inspiron-1750.(none)>
2016-08-01 16:01:34 +0200

    lsarp.h

---
+++ lsarp.h
@@ -0,0 +1,14 @@
+/** @file
+ */
+
+#ifndef __LWIP_LSARP_H__
+#define __LWIP_LSARP_H__
+
+
+#define VERSION 1 // 0=Orginal protocal 1=Modified protocol
+#define KEY1 5
+#define KEY2 5
+
+#endif /* LWIP_DHCP */
+
+#endif /*__LWIP_DHCP_H__*/
